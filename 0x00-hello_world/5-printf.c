10a9a7bec30fc0ff2ea61f9e4dc3a07b452d7ba9
unknown <Administrator@frans.etre.com>
2023-02-19 11:21:27 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main
+ * a program that prints a text line with printf
+ *return 0:(Seccuss)
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
