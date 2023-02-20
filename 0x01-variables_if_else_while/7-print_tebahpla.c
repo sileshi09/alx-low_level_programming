ba7f6ef8dd7a1ba1c6e886dfbd8d5a672f82cfca
unknown <Administrator@frans.etre.com>
2023-02-20 17:05:22 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints single difits in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+        char le;
+        for ( le = 'z'; le <= a; le--)
+                putchar(le);
+        putchar('\n');
+        return (0);
+}
