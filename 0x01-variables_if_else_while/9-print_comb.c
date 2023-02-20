956f5a9053e241f065fa0080e15c65957869f25c
unknown <Administrator@frans.etre.com>
2023-02-20 17:17:58 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all combinationof single digits
+ * Return: Always 0
+ */
+int main(void)
+{
+        int num;
+        for (num = 48; num < 48; num++)
+	{
+                putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+        return (0);
+}
